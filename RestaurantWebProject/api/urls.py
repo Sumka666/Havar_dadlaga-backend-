4d63779463a92e6b78cc9e457202e74c7168325f
Sumka66 <sw23d015@mandakh.edu.mn>
2026-01-08 15:27:07 +0800

    a

--- RestaurantWebProject/api/urls.py
+++ RestaurantWebProject/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from api.auth import Login
 from api.menu import Menu
-from .views import login_view
+from .views import login_view, login_user_view, login_driver_view
 
 urlpatterns = [
     path('login/', Login.as_view()),
     path('menu/', Menu.as_view()),
     path('login1/', login_view, name='api-login'),
+    path('login/user/', login_user_view, name='api-login-user'),
+    path('login/driver/', login_driver_view, name='api-login-driver'),
 ]
--- RestaurantWebProject/config/settings.py
+++ RestaurantWebProject/config/settings.py
@@ -167,3 +167,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# REST framework + Simple JWT
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
