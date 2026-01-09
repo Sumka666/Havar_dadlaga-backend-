99087eec21de0c002fd2918e2e12decfbafd27ec
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2026-01-09 10:47:46 +0800

    render

--- RestaurantWebProject/config/settings.py
+++ RestaurantWebProject/config/settings.py
@@ -36,8 +36,12 @@ SECRET_KEY = 'django-insecure-+dmlvofx^nn8=dd8xtei#imq2e@f=f^wc-y+s4i8hv2=ez^62@
 DEBUG = True
 
 # ALLOWED_HOSTS = ['.onrender.com']
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ['.onrender.com']
+    
 # Application definition
 
 INSTALLED_APPS = [
